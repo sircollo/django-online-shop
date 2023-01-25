14f82108f3c88d70f35d600357882fc14d86f0dd
sircollo <sircollo71@gmail.com>
2023-01-26 01:08:13 +0300

    edit product and category models to get absolute url of a given object

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -15,6 +15,10 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', 
+                       args=[self.slug])
     
 class Product(models.Model):
     category = models.ForeignKey(Category, 
@@ -41,3 +45,7 @@ class Product(models.Model):
         
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', 
+                       args=[self.id, self.slug])
